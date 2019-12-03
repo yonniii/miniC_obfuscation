f7a66dcad973be6d140195f0f7c08d02cbb157c3
yonnii <rkdtjdus753@gmail.com>
2019-12-03 20:42:45 +0900

    init project

---
+++ src/listener/BytecodeGenListener.java
@@ -0,0 +1,464 @@
+package listener;
+
+import generated.MiniCBaseListener;
+import generated.MiniCParser;
+import generated.MiniCParser.ParamsContext;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+import org.antlr.v4.runtime.tree.ParseTreeProperty;
+
+import static listener.BytecodeGenListenerHelper.*;
+import static listener.SymbolTable.Type;
+
+
+public class BytecodeGenListener extends MiniCBaseListener implements ParseTreeListener {
+    ParseTreeProperty<String> newTexts = new ParseTreeProperty<String>();
+    SymbolTable symbolTable = new SymbolTable();
+
+    int tab = 0;
+    int label = 0;
+
+    // program	: decl+
+
+    @Override
+    public void enterFun_decl(MiniCParser.Fun_declContext ctx) {
+        symbolTable.initFunDecl();
+
+        String fname = getFunName(ctx);
+        ParamsContext params;
+
+        if (fname.equals("main")) { //함수가 main인 경우, args 파라미터를 테이블에 삽입
+            symbolTable.putLocalVar("args", Type.INTARRAY);
+        } else { //main이 아닌 경우
+            symbolTable.putFunSpecStr(ctx); //함수의 선언부?를 bytecode로 하여 테이블에 담는 함수 실행
+            params = (MiniCParser.ParamsContext) ctx.getChild(3);
+            symbolTable.putParams(params); //파라미터를 테이블에 넣는 함수 실행
+        }
+    }
+
+
+    // var_decl	: type_spec IDENT ';' | type_spec IDENT '=' LITERAL ';'|type_spec IDENT '[' LITERAL ']' ';'
+    @Override
+    public void enterVar_decl(MiniCParser.Var_declContext ctx) {
+        String varName = ctx.IDENT().getText();
+
+        if (isArrayDecl(ctx)) {
+            symbolTable.putGlobalVar(varName, Type.INTARRAY);
+        } else if (isDeclWithInit(ctx)) {
+            symbolTable.putGlobalVarWithInitVal(varName, Type.INT, initVal(ctx));
+        } else { // simple decl
+            symbolTable.putGlobalVar(varName, Type.INT);
+        }
+    }
+
+
+    @Override
+    public void enterLocal_decl(MiniCParser.Local_declContext ctx) {
+        if (isArrayDecl(ctx)) {
+            symbolTable.putLocalVar(getLocalVarName(ctx), Type.INTARRAY);
+        } else if (isDeclWithInit(ctx)) {
+            symbolTable.putLocalVarWithInitVal(getLocalVarName(ctx), Type.INT, initVal(ctx));
+        } else { // simple decl
+            symbolTable.putLocalVar(getLocalVarName(ctx), Type.INT);
+        }
+    }
+
+
+    @Override
+    public void exitProgram(MiniCParser.ProgramContext ctx) {
+        String classProlog = getFunProlog();
+
+        String fun_decl = "", var_decl = "";
+
+        for (int i = 0; i < ctx.getChildCount(); i++) {
+            if (isFunDecl(ctx, i))
+                fun_decl += newTexts.get(ctx.decl(i));
+            else
+                var_decl += newTexts.get(ctx.decl(i));
+        }
+
+        newTexts.put(ctx, classProlog + var_decl + fun_decl);
+
+        System.out.println(newTexts.get(ctx));
+    }
+
+
+    // decl	: var_decl | fun_decl
+    @Override
+    public void exitDecl(MiniCParser.DeclContext ctx) {
+        String decl = "";
+        if (ctx.getChildCount() == 1) {
+            if (ctx.var_decl() != null)                //var_decl
+                decl += newTexts.get(ctx.var_decl());
+            else                            //fun_decl
+                decl += newTexts.get(ctx.fun_decl());
+        }
+        newTexts.put(ctx, decl);
+    }
+
+    // stmt	: expr_stmt | compound_stmt | if_stmt | while_stmt | return_stmt
+    @Override
+    public void exitStmt(MiniCParser.StmtContext ctx) {
+        String stmt = "";
+        if (ctx.getChildCount() > 0) { //각자 stmt에 맞는 함수를 실행하여 반환된 값을 newText에 넣음.
+            if (ctx.expr_stmt() != null)                // expr_stmt
+                stmt += newTexts.get(ctx.expr_stmt());
+            else if (ctx.compound_stmt() != null)    // compound_stmt
+                stmt += newTexts.get(ctx.compound_stmt());
+            else if (ctx.if_stmt() != null)
+                stmt += newTexts.get(ctx.if_stmt());
+            else if (ctx.while_stmt() != null)
+                stmt += newTexts.get(ctx.while_stmt());
+            else if (ctx.return_stmt() != null)
+                stmt += newTexts.get(ctx.return_stmt());
+        }
+        newTexts.put(ctx, stmt);
+    }
+
+    // expr_stmt	: expr ';'
+    @Override
+    public void exitExpr_stmt(MiniCParser.Expr_stmtContext ctx) {
+        String stmt = "";
+        if (ctx.getChildCount() == 2) {
+            stmt += newTexts.get(ctx.expr());    // expr
+        }
+        newTexts.put(ctx, stmt);
+    }
+
+
+    // while_stmt	: WHILE '(' expr ')' stmt
+    @Override
+    public void exitWhile_stmt(MiniCParser.While_stmtContext ctx) {
+        String stmt = "";
+        String condExpr = newTexts.get(ctx.expr()); // while의 조건문
+        String loopStmt = newTexts.get(ctx.stmt()); // 반복할 stmt
+
+        String lLoop = symbolTable.newLabel(); // 되돌아갈 라벨
+        String lBreak = symbolTable.newLabel(); // 조건에 만족하지 않을 때 빠져나갈 라벨
+
+        stmt += lLoop + ":" + "\n" //먼저 되돌아갈 라벨을 명시하여 되돌아왔을 때 조건검사 하도록 함
+                + condExpr // 조건 검사 실행
+                + "ifeq " + lBreak + "\n" //조건 검사 결과가 0이라면 빠져나감
+                + loopStmt // 조건검사 결과가 0이 아닌 경우 실행할 stmt
+                + "goto " + lLoop + "\n" // 다시 조건 검사를 하도록 loop라벨로 돌아감
+                + lBreak + ":" + "\n"; //조건 검사 결과가 0일 때 빠져나갈 라벨
+
+        newTexts.put(ctx, stmt);
+    }
+
+    @Override
+    public void exitFun_decl(MiniCParser.Fun_declContext ctx) {
+//        symbolTable.putFunSpecStr(ctx);
+        String fHeader = funcHeader(ctx, getFunName(ctx));  // 함수의 선언부를 가져오고 메모리 할당까지 함
+        String compStmt = newTexts.get(ctx.compound_stmt()); // stmt부분을 가져옴
+        String endF = "";
+        if (isVoidF(ctx) && ctx.compound_stmt().stmt(ctx.compound_stmt().stmt().size() - 1).return_stmt() == null) { //void인 경우 return문이 없을 때 return 하는 코드 추가
+            endF += "return\n";
+        }
+        endF += ".end method\n"; //함수의 마지막 부분
+        newTexts.put(ctx, fHeader + compStmt + endF);
+    }
+
+
+    private String funcHeader(MiniCParser.Fun_declContext ctx, String fname) {
+        return ".method public static " + symbolTable.getFunSpecStr(fname) + "\n"
+                + "\t" + ".limit stack " + getStackSize(ctx) + "\n"
+                + "\t" + ".limit locals " + getLocalVarSize(ctx) + "\n";
+
+    }
+
+
+    @Override
+    public void exitVar_decl(MiniCParser.Var_declContext ctx) {
+        String varName = ctx.IDENT().getText();
+        String varDecl = "";
+
+        if (isDeclWithInit(ctx)) {
+            varDecl += "putfield " + varName + "\n";
+            // v. initialization => Later! skip now..:
+        }
+        newTexts.put(ctx, varDecl);
+    }
+
+
+    @Override
+    public void exitLocal_decl(MiniCParser.Local_declContext ctx) {
+        String varDecl = "";
+
+        if (isDeclWithInit(ctx)) {
+            String vId = symbolTable.getVarId(ctx);
+            varDecl += "ldc " + ctx.LITERAL().getText() + "\n"
+                    + "istore " + vId + "\n";
+        }
+
+        newTexts.put(ctx, varDecl);
+    }
+
+
+    // compound_stmt	: '{' local_decl* stmt* '}'
+    @Override
+    public void exitCompound_stmt(MiniCParser.Compound_stmtContext ctx) {
+        String stmt = "";
+        for (int i = 0; i < ctx.local_decl().size(); i++) { //n개의 local변수 선언 코드들을 가져옴
+            stmt += newTexts.get(ctx.local_decl(i));
+        }
+
+        for (int i = 0; i < ctx.stmt().size(); i++) { // n개의 stmt부분을 가져옴
+            stmt += newTexts.get(ctx.stmt(i));
+        }
+
+        newTexts.put(ctx, stmt);
+    }
+
+    // if_stmt	: IF '(' expr ')' stmt | IF '(' expr ')' stmt ELSE stmt;
+    @Override
+    public void exitIf_stmt(MiniCParser.If_stmtContext ctx) {
+        String stmt = "";
+        String condExpr = newTexts.get(ctx.expr());
+        String thenStmt = newTexts.get(ctx.stmt(0));
+
+        String lend = symbolTable.newLabel();
+        String lelse = symbolTable.newLabel();
+
+
+        if (noElse(ctx)) {
+            stmt += condExpr + "\n"
+                    + "ifeq " + lend + "\n"
+                    + thenStmt + "\n"
+                    + lend + ":" + "\n";
+        } else {
+            String elseStmt = newTexts.get(ctx.stmt(1));
+            stmt += condExpr + "\n"
+                    + "ifeq " + lelse + "\n"
+                    + thenStmt + "\n"
+                    + "goto " + lend + "\n"
+                    + lelse + ": " + elseStmt + "\n"
+                    + lend + ":" + "\n";
+        }
+
+        newTexts.put(ctx, stmt);
+    }
+
+
+    // return_stmt	: RETURN ';' | RETURN expr ';'
+    @Override
+    public void exitReturn_stmt(MiniCParser.Return_stmtContext ctx) {
+        String stmt = "";
+        if (isIntReturn(ctx)) { // int return인 경우
+            String expr = newTexts.get(ctx.expr()); // 리턴할 expr을 받아옴
+            stmt += expr + "ireturn \n"; // expr과 함께 리턴
+        } else if (isVoidReturn(ctx)) {
+            stmt += "return \n"; //리턴문 추가
+        }
+        newTexts.put(ctx, stmt);
+    }
+
+
+    @Override
+    public void exitExpr(MiniCParser.ExprContext ctx) {
+        String expr = "";
+
+        if (ctx.getChildCount() <= 0) {
+            newTexts.put(ctx, "");
+            return;
+        }
+
+        if (ctx.getChildCount() == 1) { // IDENT | LITERAL
+            if (ctx.IDENT() != null) {
+                String idName = ctx.IDENT().getText();
+                if (symbolTable.getVarType(idName) == Type.INT) {
+                    expr += "iload " + symbolTable.getVarId(idName) + " \n";
+                }
+                //else	// Type int array => Later! skip now..
+                //	expr += "           lda " + symbolTable.get(ctx.IDENT().getText()).value + " \n";
+            } else if (ctx.LITERAL() != null) {
+                String literalStr = ctx.LITERAL().getText();
+                expr += "ldc " + literalStr + " \n";
+            }
+        } else if (ctx.getChildCount() == 2) { // UnaryOperation
+            expr = handleUnaryExpr(ctx, expr);
+            if (ctx.getChild(0).getText().equals("++") || ctx.getChild(0).getText().equals("--")) { // ++, --인 경우 istore해야 하므로 해당 코드 추가함
+                expr += "istore " + symbolTable.getVarId(ctx.expr(0).getText()) + " \n";
+            }
+//            expr = handleUnaryExpr(ctx, newTexts.get(ctx) + expr);
+        } else if (ctx.getChildCount() == 3) {
+            if (ctx.getChild(0).getText().equals("(")) {        // '(' expr ')'
+                expr = newTexts.get(ctx.expr(0));
+
+            } else if (ctx.getChild(1).getText().equals("=")) {    // IDENT '=' expr
+                expr = newTexts.get(ctx.expr(0))
+                        + "istore " + symbolTable.getVarId(ctx.IDENT().getText()) + " \n";
+
+            } else {                                            // binary operation
+                expr = handleBinExpr(ctx, expr);
+
+            }
+        }
+        // IDENT '(' args ')' |  IDENT '[' expr ']'
+        else if (ctx.getChildCount() == 4) {
+            if (ctx.args() != null) {        // function calls
+                expr = handleFunCall(ctx, expr);
+            } else { // expr
+                // Arrays: TODO
+            }
+        }
+        // IDENT '[' expr ']' '=' expr
+        else { // Arrays: TODO			*/
+        }
+        newTexts.put(ctx, expr);
+    }
+
+
+    private String handleUnaryExpr(MiniCParser.ExprContext ctx, String expr) {
+        String l1 = symbolTable.newLabel();
+        String l2 = symbolTable.newLabel();
+        String lend = symbolTable.newLabel();
+
+        expr += newTexts.get(ctx.expr(0));
+        switch (ctx.getChild(0).getText()) {
+            case "-":
+                expr += "ineg \n";
+                break;
+            case "--":
+                expr += "ldc 1" + "\n"
+                        + "isub" + "\n";
+                break;
+            case "++":
+                expr += "ldc 1" + "\n"
+                        + "iadd" + "\n";
+                break;
+            case "!":
+                expr += "ifeq " + l2 + "\n"
+                        + l1 + ": " + "ldc 0" + "\n"
+                        + "goto " + lend + "\n"
+                        + l2 + ": " + "ldc 1" + "\n"
+                        + lend + ": " + "\n";
+                break;
+        }
+        return expr;
+    }
+
+
+    private String handleBinExpr(MiniCParser.ExprContext ctx, String expr) {
+        String l2 = symbolTable.newLabel();
+        String lend = symbolTable.newLabel();
+
+        expr += newTexts.get(ctx.expr(0));
+        expr += newTexts.get(ctx.expr(1));
+
+        switch (ctx.getChild(1).getText()) {
+            case "*":
+                expr += "imul \n";
+                break;
+            case "/":
+                expr += "idiv \n";
+                break;
+            case "%":
+                expr += "irem \n";
+                break;
+            case "+":        // expr(0) expr(1) iadd
+                expr += "iadd \n";
+                break;
+            case "-":
+                expr += "isub \n";
+                break;
+//            case "==":
+//                expr += "if_cmpne ";
+//                break;
+//            case "!=":
+//                expr += "if_cmpeq ";
+//                break;
+            case "==":
+                expr += "isub " + "\n"
+                        + "ifeq " + l2 + "\n"
+                        + "ldc 0" + "\n"
+                        + "goto " + lend + "\n"
+                        + l2 + ": \n" + "ldc 1" + "\n"
+                        + lend + ": " + "\n";
+                break;
+            case "!=":
+                expr += "isub " + "\n"
+                        + "ifne " + l2 + "\n"
+                        + "ldc 0" + "\n"
+                        + "goto " + lend + "\n"
+                        + l2 + ": \n" + "ldc 1" + "\n"
+                        + lend + ": " + "\n";
+                break;
+            case "<=": // 뺐을 때 0보다 작거나 같은 경우
+                expr += "isub " + "\n"
+                        + "ifle " + l2 + "\n"
+                        + "ldc 0" + "\n"
+                        + "goto " + lend + "\n"
+                        + l2 + ": \n" + "ldc 1" + "\n"
+                        + lend + ": " + "\n";
+                break;
+            case "<": //뺐을 때 0보다 작은 경우
+                expr += "isub " + "\n"
+                        + "iflt " + l2 + "\n"
+                        + "ldc 0" + "\n"
+                        + "goto " + lend + "\n"
+                        + l2 + ": \n" + "ldc 1" + "\n"
+                        + lend + ": " + "\n";
+                break;
+
+            case ">=": // 뺏을 때 0보다 크거나 같은 경우
+                expr += "isub " + "\n"
+                        + "ifge " + l2 + "\n"
+                        + "ldc 0" + "\n"
+                        + "goto " + lend + "\n"
+                        + l2 + ": \n" + "ldc 1" + "\n"
+                        + lend + ": " + "\n";
+                break;
+
+            case ">": //뺐을 때 0보다 큰 경우
+                expr += "isub " + "\n"
+                        + "ifgt " + l2 + "\n"
+                        + "ldc 0" + "\n"
+                        + "goto " + lend + "\n"
+                        + l2 + ": \n" + "ldc 1" + "\n"
+                        + lend + ": " + "\n";
+                break;
+
+            case "and":
+                expr += "ifne " + lend + "\n"
+                        + "pop" + "\n" + "ldc 0" + "\n"
+                        + lend + ": " + "\n";
+                break;
+            case "or":
+                expr += "ifeq " + lend + "\n" //0이라면  lend로
+                        + "pop" + "\n" + "ldc 1" + "\n" //0이 아니라면 pop하고 1을 스택에
+                        + lend + ": " + "\n";
+                break;
+
+        }
+        return expr;
+    }
+
+    private String handleFunCall(MiniCParser.ExprContext ctx, String expr) {
+        String fname = getFunName(ctx);
+
+        if (fname.equals("_print")) {        // System.out.println
+            expr = "getstatic java/lang/System/out Ljava/io/PrintStream; " + "\n"
+                    + newTexts.get(ctx.args())
+                    + "invokevirtual " + symbolTable.getFunSpecStr("_print") + "\n";
+        } else {
+            expr = newTexts.get(ctx.args())
+                    + "invokestatic " + getCurrentClassName() + "/" + symbolTable.getFunSpecStr(fname) + "\n";
+        }
+
+        return expr;
+
+    }
+
+    // args	: expr (',' expr)* | ;
+    @Override
+    public void exitArgs(MiniCParser.ArgsContext ctx) {
+
+        String argsStr = "\n";
+
+        for (int i = 0; i < ctx.expr().size(); i++) {
+            argsStr += newTexts.get(ctx.expr(i));
+        }
+        newTexts.put(ctx, argsStr);
+    }
+
+}
---
+++ src/listener/BytecodeGenListenerHelper.java
@@ -0,0 +1,130 @@
+package listener;
+
+import generated.MiniCParser;
+import generated.MiniCParser.*;
+
+public class BytecodeGenListenerHelper {
+
+    // <boolean functions>
+
+    static boolean isFunDecl(MiniCParser.ProgramContext ctx, int i) {
+        return ctx.getChild(i).getChild(0) instanceof MiniCParser.Fun_declContext;
+    }
+
+    // type_spec IDENT '[' ']'
+    static boolean isArrayParamDecl(ParamContext param) {
+        return param.getChildCount() == 4;
+    }
+
+    // global vars
+    static int initVal(Var_declContext ctx) {
+        return Integer.parseInt(ctx.LITERAL().getText());
+    }
+
+    // var_decl	: type_spec IDENT '=' LITERAL ';
+    static boolean isDeclWithInit(Var_declContext ctx) {
+        return ctx.getChildCount() == 5;
+    }
+
+    // var_decl	: type_spec IDENT '[' LITERAL ']' ';'
+    static boolean isArrayDecl(Var_declContext ctx) {
+        return ctx.getChildCount() == 6;
+    }
+
+    // <local vars>
+    // local_decl	: type_spec IDENT '[' LITERAL ']' ';'
+    static int initVal(Local_declContext ctx) {
+        return Integer.parseInt(ctx.LITERAL().getText());
+    }
+
+    static boolean isArrayDecl(Local_declContext ctx) {
+        return ctx.getChildCount() == 6;
+    }
+
+    static boolean isDeclWithInit(Local_declContext ctx) {
+        return ctx.getChildCount() == 5;
+    }
+
+    static boolean isVoidF(Fun_declContext ctx) {
+        if("void".equals(ctx.type_spec().getText()))
+        	return true;
+        else
+        	return false;
+    }
+
+    static boolean isIntReturn(MiniCParser.Return_stmtContext ctx) {
+        return ctx.getChildCount() == 3;
+    }
+
+
+    static boolean isVoidReturn(MiniCParser.Return_stmtContext ctx) {
+        return ctx.getChildCount() == 2;
+    }
+
+    // <information extraction>
+    static String getStackSize(Fun_declContext ctx) {
+        return "32";
+    }
+
+    static String getLocalVarSize(Fun_declContext ctx) {
+        return "32";
+    }
+
+    static String getTypeText(Type_specContext typespec) {
+    	if(typespec.VOID() != null)
+    		return "V";
+    	else if(typespec.INT() != null)
+    		return "I";
+    	return null;
+    }
+
+    // params
+    static String getParamName(ParamContext param) {
+        return param.IDENT().getText();
+    }
+
+    static String getParamTypesText(ParamsContext params) {
+        String typeText = "";
+
+        for (int i = 0; i < params.param().size(); i++) {
+            MiniCParser.Type_specContext typespec = (MiniCParser.Type_specContext) params.param(i).getChild(0);
+            typeText += getTypeText(typespec); // + ";";
+        }
+        return typeText;
+    }
+
+    static String getLocalVarName(Local_declContext local_decl) {
+        return local_decl.IDENT().getText();
+    }
+
+    static String getFunName(Fun_declContext ctx) {
+        return ctx.IDENT().getText();
+    }
+
+    static String getFunName(ExprContext ctx) {
+//        if (ctx.getChild(1).getText() != "(") {
+//            return null;
+//        }
+        return ctx.IDENT().getText();
+    }
+
+    static boolean noElse(If_stmtContext ctx) {
+        return ctx.getChildCount() <= 5;
+    }
+
+    static String getFunProlog() {
+		String init =
+				".class public " + getCurrentClassName() + "\n" +
+						".super java/lang/Object\n" +
+						".method public <init>()V\n" +
+						"aload_0\n" +
+						"invokenonvirtual java/lang/Object/<init>()V\n" +
+						"return\n" +
+						".end method" + "\n";
+		return init;
+    }
+
+    static String getCurrentClassName() {
+        return "Test";
+    }
+}
